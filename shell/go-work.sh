ee1604bd671fb7f67504042a82c6740b222d16d6
b <b@b.b>
2023-08-02 01:46:33 +0000

    update go work

--- shell/go-work.sh
+++ shell/go-work.sh
@@ -5,5 +5,8 @@ cd work
 git clone git@github.com:5-say/go-tools.git
 go work use ./go-tools
 
-git clone git@github.com:5-say/zero-services.git
-go work use ./zero-services
+git clone git@github.com:5-say/zero-auth.git
+go work use ./zero-auth
+
+git clone git@github.com:zero-vue-demo/app-center-public.git
+go work use ./app-center-public
